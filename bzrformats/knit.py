a44c230042362ce792457acb9df0726000c91619
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-04-13 02:08:28 +0100

    Delegate knit_network_to_record header parsing to Rust

--- bzrformats/knit.py
+++ bzrformats/knit.py
@@ -645,28 +645,12 @@ def knit_network_to_record(storage_kind, bytes, line_end):
     :param storage_kind: The storage kind of the record.
     :param bytes: The bytes of the record on the network.
     """
-    start = line_end
-    line_end = bytes.find(b"\n", start)
-    key = tuple(bytes[start:line_end].split(b"\x00"))
-    start = line_end + 1
-    line_end = bytes.find(b"\n", start)
-    parent_line = bytes[start:line_end]
-    if parent_line == b"None:":
-        parents = None
-    else:
-        parents = tuple(
-            [
-                tuple(segment.split(b"\x00"))
-                for segment in parent_line.split(b"\t")
-                if segment
-            ]
-        )
-    start = line_end + 1
-    noeol = bytes[start : start + 1] == b"N"
+    key, parents, noeol, raw_offset = _knit_rs.parse_network_record_header_rs(
+        bytes, line_end
+    )
     method = "fulltext" if "ft" in storage_kind else "line-delta"
     build_details = (method, noeol)
-    start = start + 1
-    raw_record = bytes[start:]
+    raw_record = bytes[raw_offset:]
     annotated = "annotated" in storage_kind
     return [
         KnitContentFactory(
